fd10e5b4f3425fcc6e6177a72f0c3b773434b07e
John Spielman <hondajohn88@gmail.com>
2016-01-13 20:00:14 -0800

    Revert '[2/2] Base: Add lockscreen rotation as an optional rotation setting'

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -420,9 +420,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.BATTERY_SAVER_MODE_COLOR),
                     false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.LOCKSCREEN_ROTATION),
-                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -438,11 +435,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         mBatterySaverWarningColor = mContext.getResources()
                                 .getColor(com.android.internal.R.color.battery_saver_mode_color);
                     }
-            } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.LOCKSCREEN_ROTATION))
-                    || uri.equals(Settings.System.getUriFor(
-                    Settings.System.ACCELEROMETER_ROTATION))) {
-                mStatusBarWindowManager.updateKeyguardScreenRotation();
             }
             update();
         }
